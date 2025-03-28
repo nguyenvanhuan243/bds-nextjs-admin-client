8215015277fdfdec012c570a16289649124c0960
Huan Nguyen <nguyenvanhuan243@gmail.com>
2025-03-28 10:05:31 +0700

    Upodae

--- src/components/Layouts/header/index.tsx
+++ src/components/Layouts/header/index.tsx
@@ -38,7 +38,9 @@ export function Header() {
         <h1 className="mb-0.5 text-heading-5 font-bold text-dark dark:text-white">
           Dashboard
         </h1>
-        <p className="font-medium">Next.js Admin Dashboard Solution</p>
+        <p className="font-medium">
+          Quản lí cửa hàng trang sức
+        </p>
       </div>
 
       <div className="flex flex-1 items-center justify-end gap-2 min-[375px]:gap-4">
